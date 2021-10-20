cab9258e1460758785342b94672d61ab3ab8850f
Nikolay Zverev <zverevnn@yandex.ru>
2021-10-21 01:03:24 +0500

    calc v 2.0

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ $( "#datepicker" ).datepicker({
 
 let methods = new Methods();
 
+// настраиваем работу слайдера (бегунка)
 let sliderValue = {
     min: 1000,
     max: 3000000,
@@ -54,6 +55,10 @@ function removeSpaces(value) {
 
 let formCalculator = document.getElementById('form-calculator');
 let calculationDetailsButton = document.getElementById('calculation-details-button');
+
+// запускаем класс для показа подробной информации по вкладу в разрезе месяцев
+// передаем кнопку-элемент, при нажатии на которую, соответствующий блок будет изменять свою видимость
+// передаем координаты для блока 
 let infobox = new Infobox({
     toggleElem: calculationDetailsButton,
     defaultCoordinates : {
@@ -64,6 +69,7 @@ let infobox = new Infobox({
 
 
 function getInputData() {
+    // получить актуальные данные со всех input
     let result = {
         depositDate: $('#datepicker').val(),
         depositAmount: removeSpaces( $('#deposit-amount-input').val() ),
@@ -78,33 +84,69 @@ function getInputData() {
 
 $(document).ready(function() {
 
+    // сразу после готовности DOM рассчитываем и показываем результат
+    // с дефолтными расчетными данными 
     processDataAjax(getInputData());
 
     let formCalculator = $('#form-calculator');
 
-
+    // при событии submit рассчитываем и показываем результат
+    // с входными данными, которые указал/не указал пользователь в соответствующих input 
     formCalculator.submit(function(event) {
         event.preventDefault();
-        
         processDataAjax(getInputData());
-
     })
 });
 
+function setResultLabel({
+    // функция показывает результат вычислений или ошибку 
+
+    title = "Результат: ", 
+    value = '', 
+    error = false}) {
+
+    $('#result-value .result-label').text(title);
+    $('#result-value .result-value').text(value);
+
+    if (error) {
+        $('#result-value').addClass("form-data-error");
+    } else {
+        $('#result-value').removeClass("form-data-error");
+    }
+}
 
 function processDataAjax(inputData) {
+    // отправка/получение/обработка данных от calc.php
+
     $.ajax({
         method: 'POST',
         url: '../calc.php',
         data: inputData,
     })
     .done(function(answer){
-        //console.log(answer);
         
-        let result = (answer) ?  JSON.parse(answer) : '';
+        // в переменной result ожидаем объект 
+        // с результатами вычислений или подробной информацией об ошибке
+        // или false, если данные не получены
+
+        let result = (answer) ?  JSON.parse(answer) : false;
+
+        if (!result) {
+            setResultLabel({
+                title: 'Неизвестная ошибка', 
+                error: true
+            });
+
+            infobox.calculate({error : true});
+            calculationDetailsButton.hidden = true;
+            return;
+        }
 
         calculationDetailsButton.hidden = result.error;
 
+        // запускаем соответствующий метод класса для генерации блока 
+        // с подробной информацией по капитализации вклада по месяцам 
+        // в случае result.error == true, метод отреагирует нужным образом
         infobox.calculate({
             error : result.error,
             tableRows : result.value.details,
@@ -114,18 +156,15 @@ function processDataAjax(inputData) {
             capitalization : result.value.capitalization
         });
 
-        
-        if (result.error) {
-            $('#result-value').addClass("form-data-error");
-        } else {
-            $('#result-value').removeClass("form-data-error");
-        }
-        
         let resultTitle = (result.error) ? 'Ошибка! ' : 'Результат: ';
         let resultValue = (result.error) ? result.value[0] : infobox.finalDepositAmountFormatRub;
 
-        $('#result-value .result-label').text(resultTitle);
-        $('#result-value .result-value').text(resultValue);
+        // указываем результат вычислений или текст ошибки
+        setResultLabel({
+            value: resultValue, 
+            title: resultTitle, 
+            error: result.error
+        });
         
     });
 }
